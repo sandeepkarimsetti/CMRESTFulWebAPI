2efa2bdaef623e98576328d9f29b0e5bcbed7d81
ManiKrishna <ManiKrishna@192.168.1.5>
2018-05-01 21:46:54 +0530

    Added Cross Origin Annotation

--- CMRESTFulWebAPI_Remote/src/main/java/com/contactsmanager/controller/ContactsManagerController.java
+++ CMRESTFulWebAPI_Remote/src/main/java/com/contactsmanager/controller/ContactsManagerController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.contactsmanager.domain.*;
 import com.contactsmanager.service.ContactsManagerService;
 import com.contactsmanager.service.ContactsManagerServiceImpl;
-
+@CrossOrigin
 @RestController
 public class ContactsManagerController {
 	
