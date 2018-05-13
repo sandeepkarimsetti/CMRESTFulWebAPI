5ad3ee7621966d862f67a94e8abbfaf82a6e9d1a
ManiKrishna <ManiKrishna@192.168.1.5>
2018-05-13 08:59:48 +0530

    Added ResponseEntity to all controller methods

--- CMRESTFulWebAPI_Remote/src/main/java/com/contactsmanager/controller/ContactsManagerController.java
+++ CMRESTFulWebAPI_Remote/src/main/java/com/contactsmanager/controller/ContactsManagerController.java
@@ -24,7 +24,7 @@ import com.contactsmanager.service.ContactsManagerServiceImpl;
 @RestController
 @RequestMapping(value = "/CMWebAPI")
 public class ContactsManagerController {
-	
+
 	@Autowired
 	private ContactsManagerServiceImpl contactsManagerService;
 
@@ -32,11 +32,12 @@ public class ContactsManagerController {
 		return contactsManagerService;
 	}
 
-	public void ContactsManagerServiceImpl (ContactsManagerServiceImpl contactsManagerService) {
+	public void ContactsManagerServiceImpl(ContactsManagerServiceImpl contactsManagerService) {
 		this.contactsManagerService = contactsManagerService;
 	}
-//	@CrossOrigin(origins = "*", maxAge = 3600)
-//	@CrossOrigin
+
+	// @CrossOrigin(origins = "*", maxAge = 3600)
+	// @CrossOrigin
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String defaultRequest() {
 		return "invalid request";
@@ -44,107 +45,136 @@ public class ContactsManagerController {
 
 	// GET all customers
 	// API URL: /CMWebAPI/contacts
-//	@CrossOrigin
-//	@CrossOrigin(origins = "*", maxAge = 3600)
+	// @CrossOrigin
+	// @CrossOrigin(origins = "*", maxAge = 3600)
 	@RequestMapping(value = "/contacts", method = RequestMethod.GET)
-	public List<Contact> getAllContacts() {
-		
+	public ResponseEntity<List<Contact>> getAllContacts() {
+
 		System.out.println("==== In Controller getAllContacts method ====");
-		
+
 		List<Contact> fetchedContactsList = null;
-		
-		/*allContactsList.add(new Contact("First Name", "Last Name", 9912638661L, "first.last@gmail.com", "city"));
-		allContactsList
-				.add(new Contact("First Name 1", "Last Name 1", 9912638661L, "first1.last1@gmail.com", "city 1"));
-		allContactsList
-				.add(new Contact("First Name 2", "Last Name 2", 9951178491L, "first2.last2@gmail.com", "city 2"));
-		allContactsList
-				.add(new Contact("First Name 3", "Last Name 3", 9494531402L, "first3.last3@gmail.com", "city 3"));
-		allContactsList
-				.add(new Contact("First Name 4", "Last Name 4", 9959911666L, "first4.last4@gmail.com", "city 4"));*/
+
+		HttpHeaders responseHeaders = new HttpHeaders();
+
+		responseHeaders.add("Access-Control-Allow-Origin", "*");
+		responseHeaders.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
+		responseHeaders.add("Access-Control-Max-Age", "3600");
+		responseHeaders.add("Access-Control-Allow-Headers", "x-requested-with");
+		responseHeaders.add("Access-Control-Expose-Headers",
+				"content-type,transfer-encoding,date,connection,cache-control,x-final-url,access-control-allow-origin");
 
 		fetchedContactsList = contactsManagerService.getAllContacts();
-		
-		return fetchedContactsList;
+
+		return new ResponseEntity<List<Contact>>(fetchedContactsList, responseHeaders, HttpStatus.OK);
 
 	}
 
 	// GET a customer
 	// API URL: /CMWebAPI/contacts/id
-	//@CrossOrigin(origins = "*", maxAge = 3600)
-//	@CrossOrigin
-	//@RequestMapping(value = "/contacts/{id}", method = RequestMethod.GET)
-	//public Contact getContactByID(@PathVariable("id") String contactID) {
-	public Contact getContactByID(String contactID) {
-		// edit this line
-		Contact fetchedContact = contactsManagerService.getContactByID(Long.parseLong(contactID));
-		
-		return fetchedContact;
-	}
-	
+	// @CrossOrigin(origins = "*", maxAge = 3600)
+	// @CrossOrigin
+	// @RequestMapping(value = "/contacts/{id}", method = RequestMethod.GET)
+	// public Contact getContactByID(@PathVariable("id") String contactID) {
+	/*
+	 * public Contact getContactByID(String contactID) { // edit this line Contact
+	 * fetchedContact =
+	 * contactsManagerService.getContactByID(Long.parseLong(contactID));
+	 * 
+	 * return fetchedContact; }
+	 */
+
 	@RequestMapping(value = "/contacts/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Contact> getContactResponseByID(@PathVariable("id") String contactID) {
 		// edit this line
 		Contact fetchedContact = contactsManagerService.getContactByID(Long.parseLong(contactID));
-		
+
 		HttpHeaders responseHeaders = new HttpHeaders();
-		
+
 		responseHeaders.add("Access-Control-Allow-Origin", "*");
 		responseHeaders.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
 		responseHeaders.add("Access-Control-Max-Age", "3600");
 		responseHeaders.add("Access-Control-Allow-Headers", "x-requested-with");
-		responseHeaders.add("Access-Control-Expose-Headers", "content-type,transfer-encoding,date,connection,cache-control,x-final-url,access-control-allow-origin");
-		
+		responseHeaders.add("Access-Control-Expose-Headers",
+				"content-type,transfer-encoding,date,connection,cache-control,x-final-url,access-control-allow-origin");
+
 		return new ResponseEntity<Contact>(fetchedContact, responseHeaders, HttpStatus.OK);
 	}
 
 	// POST a customer
 	// API URL: /CMWebAPI/contacts
-	//@CrossOrigin(origins = "*", maxAge = 3600)
-//	@CrossOrigin
+	// @CrossOrigin(origins = "*", maxAge = 3600)
+	// @CrossOrigin
 	@RequestMapping(value = "/contacts", method = RequestMethod.POST)
-	public Contact createContact(@RequestBody Contact contact) {
+	public ResponseEntity<Contact> createContact(@RequestBody Contact contact) {
 		System.out.println("^^^^^ Request Body in createContact controller method ^^^^" + contact);
 		
-		if(contact == null) {
-			return  new Contact(new Long(0), "", "", new Long(0), "", "");
+		HttpHeaders responseHeaders = new HttpHeaders();
+
+		responseHeaders.add("Access-Control-Allow-Origin", "*");
+		responseHeaders.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
+		responseHeaders.add("Access-Control-Max-Age", "3600");
+		responseHeaders.add("Access-Control-Allow-Headers", "x-requested-with");
+		responseHeaders.add("Access-Control-Expose-Headers",
+				"content-type,transfer-encoding,date,connection,cache-control,x-final-url,access-control-allow-origin");
+
+		if (contact == null) {
+			return new ResponseEntity<Contact>(new Contact(new Long(0), "", "", new Long(0), "", ""), responseHeaders, HttpStatus.OK);
 		}
 		// edit this line
 		Contact newContact = contactsManagerService.createContact(contact);
-		
-		if(newContact != null) {
-			newContact = getContactByID(newContact.getContactID()+"");
+
+		if (newContact != null) {
+			newContact = contactsManagerService.getContactByID(newContact.getContactID());
 		}
-		
-		return newContact;
+
+		return new ResponseEntity<Contact>(newContact, responseHeaders, HttpStatus.OK);
 	}
 
 	// PUT a customer
 	// API URL: /CMWebAPI/contacts
-	//@CrossOrigin(origins = "*", maxAge = 3600)
-//	@CrossOrigin
+	// @CrossOrigin(origins = "*", maxAge = 3600)
+	// @CrossOrigin
 	@RequestMapping(value = "/contacts/{id}", method = RequestMethod.PUT)
-	public Contact updateContact(@PathVariable("id") String contactID, @RequestBody Contact contact) {
+	public ResponseEntity<Contact> updateContact(@PathVariable("id") String contactID, @RequestBody Contact contact) {
 		
-		if(contact == null) {
-			return  new Contact(new Long(0), "", "", new Long(0), "", "");
+		HttpHeaders responseHeaders = new HttpHeaders();
+
+		responseHeaders.add("Access-Control-Allow-Origin", "*");
+		responseHeaders.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
+		responseHeaders.add("Access-Control-Max-Age", "3600");
+		responseHeaders.add("Access-Control-Allow-Headers", "x-requested-with");
+		responseHeaders.add("Access-Control-Expose-Headers",
+				"content-type,transfer-encoding,date,connection,cache-control,x-final-url,access-control-allow-origin");
+
+		if (contact == null) {
+			return new ResponseEntity<Contact>(new Contact(new Long(0), "", "", new Long(0), "", ""), responseHeaders, HttpStatus.OK);
 		}
-		
+
 		// edit this line
 		Contact updatedContact = contactsManagerService.updateContact(Long.parseLong(contactID), contact);
-		
-		return updatedContact;
+
+		return new ResponseEntity<Contact>(updatedContact, responseHeaders, HttpStatus.OK);
 	}
 
 	// DELETE a customer
 	// API URL: /CMWebAPI/contacts/id
-	//@CrossOrigin(origins = "*", maxAge = 3600)
-//	@CrossOrigin
+	// @CrossOrigin(origins = "*", maxAge = 3600)
+	// @CrossOrigin
 	@RequestMapping(value = "/contacts/{id}", method = RequestMethod.DELETE)
-	public Contact deleteContact(@PathVariable("id") String contactID) {
+	public ResponseEntity<Contact> deleteContact(@PathVariable("id") String contactID) {
+		
+		HttpHeaders responseHeaders = new HttpHeaders();
+
+		responseHeaders.add("Access-Control-Allow-Origin", "*");
+		responseHeaders.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
+		responseHeaders.add("Access-Control-Max-Age", "3600");
+		responseHeaders.add("Access-Control-Allow-Headers", "x-requested-with");
+		responseHeaders.add("Access-Control-Expose-Headers",
+				"content-type,transfer-encoding,date,connection,cache-control,x-final-url,access-control-allow-origin");
+		
 		// edit this line
 		Contact deletedContact = contactsManagerService.deleteContact(Long.parseLong(contactID));
-		return deletedContact;
+		return new ResponseEntity<Contact>(deletedContact, responseHeaders, HttpStatus.OK);
 	}
 
 }
